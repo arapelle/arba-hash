93f8407450cb8405f4a8f2b481d01be8a24565b9
Aymeric Pellé <aymeric.pelle.dev@proton.me>
2026-04-22 00:35:56 +0200

    0.4.0:#7: murmur_hash_2_u64() handles range of byte spans.
    This avoids unnecessary data copying when hashing non-contiguous data.
    
    The initial seed is now calculated within dedicated functions, simplifying the main hashing functions.
    Final mixing step has also been moved to a dedicated function.

--- test/murmur_hash_2_tests.cpp
+++ test/murmur_hash_2_tests.cpp
@@ -1,4 +1,5 @@
 #include <arba/hash/murmur_hash_2.hpp>
+#include <arba/hash/murmur_hash.hpp>
 
 #include <gtest/gtest.h>
 
@@ -11,6 +12,14 @@ TEST(murmur_hash_2_tests, murmur_hash_2_u64__eq_data_specific__eq)
     std::array<std::uint8_t, 8> bytes{ 102, 26, 64, 25, 55, 224, 146, 246 };
     uint64_t hash = hash::murmur_hash_2_u64(bytes, cppx::endianness_specific);
     uint64_t hash2 = hash::murmur_hash_2_u64(bytes, cppx::endianness_specific);
+    ASSERT_EQ(hash, hash2);
+}
+
+TEST(murmur_hash_2_tests, murmur_hash_2_u64__compat_specific__eq)
+{
+    std::array<std::uint8_t, 8> bytes{ 102, 26, 64, 25, 55, 224, 146, 246 };
+    uint64_t hash = hash::murmur_hash_2_u64(bytes, cppx::endianness_specific);
+    uint64_t hash2 = hash::murmur_hash_64(bytes);
     ASSERT_EQ(hash, hash2);
 }
 
@@ -37,6 +46,19 @@ TEST(murmur_hash_2_tests, murmur_hash_2_u64__eq_ranges_specific__eq)
     std::vector<std::uint8_t> v_bytes{ 102, 26, 64, 25, 55, 224, 146, 246 };
     uint64_t hash = hash::murmur_hash_2_u64(a_bytes, cppx::endianness_specific);
     uint64_t hash2 = hash::murmur_hash_2_u64(v_bytes, cppx::endianness_specific);
+    ASSERT_EQ(hash, hash2);
+}
+
+// murmur_hash_2_u64 (endianness_specific, multi range of bytes):
+
+TEST(murmur_hash_2_tests, murmur_hash_2_u64__multi_range_specific__eq)
+{
+    std::array<std::uint8_t, 8> bytes_p1{ 102, 26, 64, 25, 55, 224, 146, 246 };
+    std::array<std::uint8_t, 10> bytes_p2{ 201, 62, 46, 52, 55, 122, 141, 207, 15, 47 };
+    std::array<std::span<std::uint8_t>, 2> bytes_range{ std::span(bytes_p1), std::span(bytes_p2) };
+    uint64_t hash = hash::murmur_hash_2_u64(bytes_range, cppx::endianness_specific);
+    std::array<std::uint8_t, 18> other_bytes{ 102, 26, 64, 25, 55, 224, 146, 246, 201, 62, 46, 52, 55, 122, 141, 207, 15, 47 };
+    uint64_t hash2 = hash::murmur_hash_2_u64(other_bytes, cppx::endianness_specific);
     ASSERT_EQ(hash, hash2);
 }
 
@@ -77,9 +99,22 @@ TEST(murmur_hash_2_tests, murmur_hash_2_u64__eq_ranges_neutral__eq)
     ASSERT_EQ(hash, hash2);
 }
 
+// murmur_hash_2_u64 (endianness_neutral, multi range of bytes):
+
+TEST(murmur_hash_2_tests, murmur_hash_2_u64__multi_range_neutral__eq)
+{
+    std::array<std::uint8_t, 8> bytes_p1{ 102, 26, 64, 25, 55, 224, 146, 246 };
+    std::array<std::uint8_t, 10> bytes_p2{ 201, 62, 46, 52, 55, 122, 141, 207, 15, 47 };
+    std::array<std::span<std::uint8_t>, 2> bytes_range{ std::span(bytes_p1), std::span(bytes_p2) };
+    uint64_t hash = hash::murmur_hash_2_u64(bytes_range, cppx::endianness_neutral);
+    std::array<std::uint8_t, 18> other_bytes{ 102, 26, 64, 25, 55, 224, 146, 246, 201, 62, 46, 52, 55, 122, 141, 207, 15, 47 };
+    uint64_t hash2 = hash::murmur_hash_2_u64(other_bytes, cppx::endianness_neutral);
+    ASSERT_EQ(hash, hash2);
+}
+
 // murmur_hash_2_16u8 (endianness_neutral):
 
-TEST(murmur_hash_tests, murmur_hash_2_16u8__eq_data_neutral__eq)
+TEST(murmur_hash_2_tests, murmur_hash_2_16u8__eq_data_neutral__eq)
 {
     std::array<std::uint8_t, 8> bytes{ 102, 26, 64, 25, 55, 224, 146, 246 };
     std::array<std::uint8_t, 16> hash = hash::murmur_hash_2_16u8(bytes, cppx::endianness_neutral);
@@ -89,7 +124,7 @@ TEST(murmur_hash_tests, murmur_hash_2_16u8__eq_data_neutral__eq)
         ASSERT_NE(hash[i], hash[i + 8]);
 }
 
-TEST(murmur_hash_tests, murmur_hash_2_16u8__ne_data_neutral__ne)
+TEST(murmur_hash_2_tests, murmur_hash_2_16u8__ne_data_neutral__ne)
 {
     std::array<std::uint8_t, 8> bytes{ 102, 26, 64, 25, 55, 224, 146, 246 };
     std::array<std::uint8_t, 16> hash = hash::murmur_hash_2_16u8(bytes, cppx::endianness_neutral);
@@ -98,7 +133,7 @@ TEST(murmur_hash_tests, murmur_hash_2_16u8__ne_data_neutral__ne)
     ASSERT_NE(hash, hash2);
 }
 
-TEST(murmur_hash_tests, murmur_hash_2_16u8__ne_seed_neutral__ne)
+TEST(murmur_hash_2_tests, murmur_hash_2_16u8__ne_seed_neutral__ne)
 {
     std::array<std::uint8_t, 8> bytes{ 102, 26, 64, 25, 55, 224, 146, 246 };
     std::array<std::uint8_t, 16> hash = hash::murmur_hash_2_16u8(bytes, cppx::endianness_neutral);
@@ -106,7 +141,7 @@ TEST(murmur_hash_tests, murmur_hash_2_16u8__ne_seed_neutral__ne)
     ASSERT_NE(hash, hash2);
 }
 
-TEST(murmur_hash_tests, murmur_hash_2_16u8__eq_ranges_neutral__eq)
+TEST(murmur_hash_2_tests, murmur_hash_2_16u8__eq_ranges_neutral__eq)
 {
     std::array<std::uint8_t, 8> a_bytes{ 102, 26, 64, 25, 55, 224, 146, 246 };
     std::vector<std::uint8_t> v_bytes{ 102, 26, 64, 25, 55, 224, 146, 246 };
